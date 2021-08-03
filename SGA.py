7fdb61ee2273c84b46834dce423ed93327495ab8
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-03 19:46:06 -0400

    Added children and mating functions

--- SGA.py
+++ SGA.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from multiprocessing import Pool
+import pandas as pd
 import numpy as np
 
 class GA:
@@ -10,7 +11,7 @@ class GA:
         self.max_features = max_features
         
         
-    def fit(self, i):
+    def fit(self, i=0):
         max_features = self.max_features if self.max_features != None else self.x.shape[1]
         rows = self.x.loc[self.pop['rows'][i] == 1].index
         cols = self.pop['cols'][i]
@@ -35,17 +36,62 @@ class GA:
                 y_test = y.loc[x_test.index]
                 y_train = y.loc[x_train.index]
                 
-                model = LogisticRegression(max_iter=500)
+                if 'solver' not in self.pop:
+                    solver = 'lbfgs'
+                else:
+                    solver = self.pop['solver'][i]
+                
+                if 'penalty' not in self.pop:
+                    penalty = 'l2'
+                else:
+                    penalty = self.pop['penalty'][i]
+                
+                model = LogisticRegression(max_iter=500, penalty=penalty, solver=solver)
             
                 model.fit(x_train, y_train)
                 predictions = model.predict(x_test)
                 fit_list.append(f1_score(y_test, predictions, average='micro'))
         
         return np.mean(fit_list)
+    
+    def mate(pop, fitness, num_parents):
+        return pop.loc[pop.index.isin(((-np.array(fitness)).argsort())[0:num_parents])]
+    
+    def children(pop, fitness, num_children, mutation_rate):
+        children = pd.DataFrame(columns=pop.columns, index=range(num_children))
+        for i in range(num_children):
+            mates = np.random.choice(np.arange(0, len(pop)), size=2, p=fitness/sum(fitness))
+            mom, dad = pop.iloc[mates[0]], pop.iloc[mates[1]]
+            
+            for j in range(len(pop.columns)):
+                if type(mom[pop.columns[j]]) == int:
+                    mutate = bool(np.random.binomial(1, mutation_rate))
+                    children.iloc[i, j] = round(np.mean([mom, dad])) if mutate else np.random.choice(mom, dad)
+                    
+                elif type(mom[pop.columns[j]]) == float:
+                    mutate = bool(np.random.binomial(1, mutation_rate))
+                    children.iloc[i, j] = np.mean([mom, dad]) if mutate else np.random.choice(mom, dad)
+                    
+                elif type(mom[pop.columns[j]]) == str:
+                    mutate = bool(np.random.binomial(1, mutation_rate))
+                    children.iloc[i, j] = np.random.choice(set(pop[pop.columns[j]])) if mutate else np.random.choice(mom, dad)
+                
+                elif type(mom[pop.columns[j]]) == np.ndarray:
+                    mutate = np.random.binomial(1, mutation_rate, len(mom[pop.columns[j]]))
+                    choice = np.random.binomial(1, 0.5, len(mom[pop.columns[j]]))
+                    child = [mom[pop.columns[j]][i] if choice[i] == 1 else dad[pop.columns[j]][i] for i in range(len(choice))]
+                    children.iloc[i, j] = np.array([1 - child[i] if mutate[i] == 1 else child[i] for i in range(len(mutate))])
+                
+                else:
+                    print('Invalid Data Type')
+                    break
+                    
+        return children
         
-    def parallel(self, workers=2):
+        
+    def parallel(self, solutions, workers=2):
         pool = Pool(workers)
-        out = pool.map(self.fit, range(10))
+        out = pool.map(self.fit, range(solutions))
         pool.close()
         pool.join()
         
