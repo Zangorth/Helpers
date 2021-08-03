03a5ffd1381dd9adf60aebc73c994af8235f9185
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-03 10:12:57 -0400

    Converting to Class for ease of use

--- SGA.py
+++ SGA.py
@@ -1,72 +1,64 @@
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.exceptions import ConvergenceWarning
-from dateutil.relativedelta import relativedelta
-from sklearn.metrics import roc_auc_score
-import pandas as pd
+from sklearn.metrics import f1_score
+from multiprocessing import Pool
 import numpy as np
-import warnings
 
-warnings.filterwarnings('error', category=ConvergenceWarning)
-
-def fitness(x, y, time, max_features, skip_time, cut_dates, model, path, solution):
-    metric_list = []
-    
-    if sum(solution['solution']) > max_features:
-        metric_list.append(-666)
+class GA:
+    def __init__(self, x, y, pop, cv=[], max_features=None):
+        self.x, self.y = x, y
+        self.pop, self.cv = pop, cv
+        self.max_features = max_features
         
-    else:
-        if model == 'logit':
-            solution = solution['solution']
-            x = pd.read_feather(path, columns = [x[i] for i in range(len(x)) if solution[i] == 1])
-            model = LogisticRegression(solver='saga', max_iter=300)
+        
+    def fit(self, i):
+        max_features = self.max_features if self.max_features != None else self.x.shape[1]
+        rows = self.x.loc[self.pop['rows'] == 1].index
+        cols = self.pop['cols']
+        
+        fit_list = []
+        
+        if sum(cols) > max_features:
+            fit_list.append(-666)
+        
+        else:
+            x = self.x.dropna()
+            y = self.y.loc[x.index]
             
-        elif model == 'boost':
-            estimator = solution['estimator']
-            learn = solution['learn']
-            depth = solution['depth']
-            solution = solution['solution']
+            x = x[[x.columns[i] for i in range(x.shape[1]) if cols[i] == 1]]
             
-            x = pd.read_feather(path, columns=[x[i] for i in range(len(x)) if solution[i] == 1])
-            model = GradientBoostingClassifier(n_estimators=estimator, learning_rate=learn, max_depth=depth)
             
-        for date in cut_dates:
-            x_test, x_train = x.loc[(time > date) & (time <= date + relativedelta(months=skip_time))], x.loc[time <= date]
-            y_test, y_train = y.loc[(time > date) & (time <= date + relativedelta(months=skip_time))], y.loc[time <= date]
+            for fold in cv:
+                x_test = x.loc[x.index.isin(fold)]
+                
+                x_train = x.loc[~x.index.isin(fold)]
+                x_train = x_train.loc[~x_train.index.isin(rows)]
+                
+                y_test = y.loc[x_test.index]
+                y_train = y.loc[x_train.index]
+                
+                model = LogisticRegression(max_iter=500)
             
-            try:
                 model.fit(x_train, y_train)
-                predictions = model.predict_proba(x_test)[:, 1]
-                metric_list.append(roc_auc_score(y_test, predictions))
-            
-            except ConvergenceWarning:
-                metric_list = [-777]
-                break
-            
-        return(np.mean(metric_list))
-    
-    
-def mate(pop, fitness, num_parents):
-    return pop[(-np.array(fitness)).argsort()][0:num_parents]
+                predictions = model.predict(x_test)
+                fit_list.append(f1_score(y_test, predictions, average='micro'))
+        
+        return np.mean(fit_list)
+        
+    def parallel(self):
+        pool = Pool()
+        out = pool.map(self.fit, range(10))
+        pool.close()
+        pool.join()
+        
+        return out
 
-def crossover(parents, offspring_size, typ='variables', mutations=3, meth='unif', mn=0, mx=0):
-    offspring = np.empty(offspring_size)
+
+ga = GA(x, y, pop, cv)
+
+if __name__ == '__main__':
+    print(ga.parallel())
     
-    for i in range(offspring_size[0]):
-        choice = np.random.choice(np.arange(0, len(parents)), size=2)
-        mom, dad = parents[choice[0]], parents[choice[1]]
-        genes = np.random.binomial(1, p=0.5, size=len(mom))
-        offspring[i] = np.where(genes == 1, mom, dad)
-        
-        prob = 0.001 if mutations/sum(offspring[i]) <= 0 else 0.999 if mutations/sum(offspring[i]) >= 1 else mutations/sum(offspring[i])
-        switch_off = np.random.binomial(1, p=prob, size=len(offspring[i]))
-        
-        prob = 0.001 if mutations/(len(offspring[i]) - sum(offspring[i])) <= 0 else 0.999 if mutations/(len(offspring[i]) - sum(offspring[i])) else mutations/(len(offspring[i]) - sum(offspring[i]))
-        switch_on = np.random.binomial(1, p=prob, size=len(offspring[i]))
-        
-        offspring[i] = np.where((offspring[i] == 1) & (switch_off == 1), 0, offspring[i])
-        offspring[i] = np.where((offspring[i] == 1) & (switch_on == 1), 1, offspring[i])
-        
-    return offspring
-        
     
+
+pop = {'rows': np.random.binomial(1, 0.95, len(x)), 'cols': np.random.binomial(1, 0.95, x.shape[1])}
+cv = [x.loc[semi == 'semi'].groupby(y, group_keys=False).apply(lambda x: x.sample(min(len(x), 25))).sort_index().index for i in range(5)]
