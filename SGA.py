6dc3523ec4c9d0efcf5da4d80c696b6899eca64c
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-03 20:37:32 -0400

    Converted some functions to use self in class

--- SGA.py
+++ SGA.py
@@ -1,14 +1,20 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.metrics import f1_score
 from multiprocessing import Pool
 import pandas as pd
 import numpy as np
+import warnings
+
+warnings.filterwarnings('error', category=ConvergenceWarning)
 
 class GA:
-    def __init__(self, x, y, pop, cv=[], max_features=None):
+    def __init__(self, x, y, pop, cv=[], model='logit', max_features=None):
         self.x, self.y = x, y
         self.pop, self.cv = pop, cv
         self.max_features = max_features
+        self.model = model
+        self.max = []
         
         
     def fit(self, i=0):
@@ -36,50 +42,56 @@ class GA:
                 y_test = y.loc[x_test.index]
                 y_train = y.loc[x_train.index]
                 
-                if 'solver' not in self.pop:
-                    solver = 'lbfgs'
-                else:
-                    solver = self.pop['solver'][i]
-                
-                if 'penalty' not in self.pop:
-                    penalty = 'l2'
-                else:
-                    penalty = self.pop['penalty'][i]
-                
-                model = LogisticRegression(max_iter=500, penalty=penalty, solver=solver)
-            
-                model.fit(x_train, y_train)
-                predictions = model.predict(x_test)
-                fit_list.append(f1_score(y_test, predictions, average='micro'))
+                if self.model == 'logit':
+                    if 'solver' not in self.pop:
+                        solver = 'lbfgs'
+                    else:
+                        solver = self.pop['solver'][i]
+                    
+                    if 'penalty' not in self.pop:
+                        penalty = 'l2'
+                    else:
+                        penalty = self.pop['penalty'][i]
+                    
+                    model = LogisticRegression(max_iter=500, penalty=penalty, solver=solver)
+                    
+                    try:
+                        model.fit(x_train, y_train)
+                        predictions = model.predict(x_test)
+                        fit_list.append(f1_score(y_test, predictions, average='micro'))
+                    except ConvergenceWarning:
+                        fit_list.append(-777)
         
         return np.mean(fit_list)
     
-    def mate(pop, fitness, num_parents):
-        return pop.loc[pop.index.isin(((-np.array(fitness)).argsort())[0:num_parents])]
+    def mate(self, fitness, num_parents):
+        self.max.append(np.max(fitness))
+        return self.pop.loc[self.pop.index.isin(((-np.array(fitness)).argsort())[0:num_parents])]
     
-    def children(pop, fitness, num_children, mutation_rate):
-        children = pd.DataFrame(columns=pop.columns, index=range(num_children))
+    def children(self, fitness, num_children, mutation_rate):
+        children = pd.DataFrame(columns=self.pop.columns, index=range(num_children))
+        
         for i in range(num_children):
-            mates = np.random.choice(np.arange(0, len(pop)), size=2, p=fitness/sum(fitness))
-            mom, dad = pop.iloc[mates[0]], pop.iloc[mates[1]]
+            mates = np.random.choice(np.arange(0, len(self.pop)), size=2, p=fitness/sum(fitness))
+            mom, dad = self.pop.iloc[mates[0]], self.pop.iloc[mates[1]]
             
-            for j in range(len(pop.columns)):
-                if type(mom[pop.columns[j]]) == int:
+            for j in range(len(self.pop.columns)):
+                if type(mom[self.pop.columns[j]]) == int:
                     mutate = bool(np.random.binomial(1, mutation_rate))
                     children.iloc[i, j] = round(np.mean([mom, dad])) if mutate else np.random.choice(mom, dad)
                     
-                elif type(mom[pop.columns[j]]) == float:
+                elif type(mom[self.pop.columns[j]]) == float:
                     mutate = bool(np.random.binomial(1, mutation_rate))
                     children.iloc[i, j] = np.mean([mom, dad]) if mutate else np.random.choice(mom, dad)
                     
-                elif type(mom[pop.columns[j]]) == str:
+                elif type(mom[self.pop.columns[j]]) == str:
                     mutate = bool(np.random.binomial(1, mutation_rate))
-                    children.iloc[i, j] = np.random.choice(set(pop[pop.columns[j]])) if mutate else np.random.choice(mom, dad)
+                    children.iloc[i, j] = np.random.choice(set(self.pop[self.pop.columns[j]])) if mutate else np.random.choice(mom, dad)
                 
-                elif type(mom[pop.columns[j]]) == np.ndarray:
-                    mutate = np.random.binomial(1, mutation_rate, len(mom[pop.columns[j]]))
-                    choice = np.random.binomial(1, 0.5, len(mom[pop.columns[j]]))
-                    child = [mom[pop.columns[j]][i] if choice[i] == 1 else dad[pop.columns[j]][i] for i in range(len(choice))]
+                elif type(mom[self.pop.columns[j]]) == np.ndarray:
+                    mutate = np.random.binomial(1, mutation_rate, len(mom[self.pop.columns[j]]))
+                    choice = np.random.binomial(1, 0.5, len(mom[self.pop.columns[j]]))
+                    child = [mom[self.pop.columns[j]][i] if choice[i] == 1 else dad[self.pop.columns[j]][i] for i in range(len(choice))]
                     children.iloc[i, j] = np.array([1 - child[i] if mutate[i] == 1 else child[i] for i in range(len(mutate))])
                 
                 else:
